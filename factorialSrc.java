262c6927497278fcc45fa3bfe4e2efa551216afb
Ayush144 <55916664+Ayush144@users.noreply.github.com>
2020-10-17 22:25:16 +0530

    Update factorialSrc.java

--- factorialSrc.java
+++ factorialSrc.java
@@ -1,25 +1,17 @@
 import java.util.*;
-class factorial{
-	static long fac(long x){
-		return fact(x+1)/(x+1); // n! = (n+1)!/(n+1)
-	}
-	
-	static long fact(long x){
-		if (x!=1)
-			return x*fact(x-1); // n! = n*(n-1)!
-		else 
-			return 1;
-	}
-	
-	public static void main(String[] args){
-		Scanner in = new Scanner(System.in);
-		long n = in.nextInt();
-		
-		if(n<0)
-			System.out.println("Math Error!\nValues less than Zero tends to Infinity!");
-		else{
-			long a = fac(n);
-			System.out.println(n+"!= "+ a);
+class Factorial 
+{
+	public static void main(String[] args) 
+	{
+		int f=1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a number");
+		int i=sc.nextInt();
+		for(;i>1;i--)
+		{
+			f=f*i;
 		}
+		System.out.println("factorial = "+f);
+
 	}
 }
